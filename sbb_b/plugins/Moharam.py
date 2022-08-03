295b91340b8840e09c52d6bf4f1ad4fc16d68d51
clipesr <106500365+clipesr@users.noreply.github.com>
2022-08-03 09:21:30 +0300

    Update and rename sasuke.py to Moharam.py

--- sbb_b/plugins/Moharam.py
+++ sbb_b/plugins/Moharam.py
@@ -14,10 +14,10 @@ except:
 plugin_category = "tools"
 
 PING_PIC = os.environ.get("PING_PIC") or (
-    "https://telegra.ph/file/39b14adf6f9d75710d673.mp4"
+    "https://telegra.ph/file/c01ad3e88c1895e032430.mp4"
 )
 
-JM_TXT = os.environ.get("PING_TEXT") or "𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 🔥"
+JM_TXT = os.environ.get("PING_TEXT") or "𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 💔"
 
 
 @sbb_b.ar_cmd(
@@ -37,7 +37,7 @@ async def _(event):
     reply_to_id = await reply_id(event)
     start = datetime.now()
     cat = await edit_or_reply(
-        event, "<b><i>  𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 🔥  </b></i>", "html"
+        event, "<b><i>  𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 💔  </b></i>", "html"
     )
     end = datetime.now()
     await cat.delete()
