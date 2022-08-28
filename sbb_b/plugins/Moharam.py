f0c7d18a2acf7a46d5eff10d94541f5e26c89719
clipesr <106500365+clipesr@users.noreply.github.com>
2022-08-29 00:59:57 +0300

    Update Moharam.py

--- sbb_b/plugins/Moharam.py
+++ sbb_b/plugins/Moharam.py
@@ -1,19 +1,7 @@
 import os
-import random
-
-from PIL import Image, ImageDraw, ImageFont
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.types import InputMessagesFilterDocument, InputMessagesFilterPhotos
+from datetime import datetime
 
 from sbb_b import sbb_b
-
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers.utils import reply_id
-from . import sbb_b, mention
-
-from datetime import datetime
 
 from . import hmention, reply_id
 
@@ -29,16 +17,33 @@ PING_PIC = os.environ.get("PING_PIC") or (
     "https://telegra.ph/file/c01ad3e88c1895e032430.mp4"
 )
 
-JM_TXT = os.environ.get("PING_TEXT") or "𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 💔"
+HSO_TXT = os.environ.get("PING_TEXT") or "𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 💔"
 
 
-@sbb_b.ar_cmd(pattern="ا ?(.*)")
-
+@sbb_b.ar_cmd(
+    pattern="$",
+    command=("", plugin_category),
+    info={
+        "header": "nothing",
+        "option": "nothing",
+        "usage": [
+            "{tr}",
+        ],
+    },
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    start = datetime.now()
+    cat = await edit_or_reply(
+        event, "<b><i>  𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 💔  </b></i>", "html"
+    )
+    end = datetime.now()
+    await cat.delete()
     ms = (end - start).microseconds / 1000
-    async def roz(rek):
-    HSO = rek.pattern_match.group(1)
-    if HSO:
-        caption = f"<b><i>{JM_TXT}<i><b>\n<code> 𝖉𝖊𝖛 ︙ @HssHH\n {HSO}"
+    if PING_PIC:
+        caption = f"<b><i>{HSO_TXT}<i><b>\n<code> 𝖉𝖊𝖛 ︙ @HssHH"
         await event.client.send_file(
             event.chat_id,
             PING_PIC,
